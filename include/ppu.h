1b775af255f3738a02c6c13aeb8b2c8375a52f58
Braydon Wang <b392wang@uwaterloo.ca>
2024-01-06 13:04:39 -0500

    set up ppu & timer

---
+++ include/ppu.h
@@ -0,0 +1,9 @@
+#ifndef __PPU_H__
+#define __PPU_H__
+
+#include <common.h>
+
+void ppu_init();
+void ppu_tick();
+
+#endif /* __PPU_H__ */
---
+++ include/timer.h
@@ -0,0 +1,9 @@
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+#include <common.h>
+
+void timer_init();
+void timer_tick();
+
+#endif /* __TIMER_H__ */
