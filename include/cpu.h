7d87320bec82b3e1413906949a9520024356f025
Braydon Wang <b392wang@uwaterloo.ca>
2024-01-07 16:10:42 -0500

    init instruction & cpu context

--- include/cpu.h
+++ include/cpu.h
@@ -2,6 +2,32 @@
 #define __CPU_H__
 
 #include <common.h>
+#include <instructions.h>
+
+typedef struct {
+    u8 a;
+    u8 f;
+    u8 b;
+    u8 c;
+    u8 d;
+    u8 e;
+    u8 h;
+    u8 l;
+    u16 pc;                 // program counter
+    u16 sp;                 // stack pointer
+} cpu_registers;
+
+typedef struct {
+    cpu_registers regs;
+    u16 fetched_data;       // currently fetched data
+    u16 mem_dest;           // memory destination
+    bool dest_is_mem;       // true if destination is memory location
+    u8 cur_opcode;          // current operation code
+    instruction *cur_inst;  // current instruction
+
+    bool halted;
+    bool stepping;
+} cpu_context;
 
 void cpu_init();
 bool cpu_step();
