5ade006bf637bf315975bfa41f8c61061fe1d1d1
Dylan Wang <d428wang@uwaterloo.ca>
2024-01-12 23:47:32 -0500

    added arithmetic instructions

--- lib/cpu_proc.c
+++ lib/cpu_proc.c
@@ -21,6 +21,10 @@ static void proc_di(cpu_context *ctx) {
     ctx->int_master_enabled = false;
 }
 
+static bool is_16_bit(reg_type rt) {
+    return rt >= RT_AF;
+}
+
 // Sets flag bits
 void cpu_set_flags(cpu_context *ctx, char z, char n, char h, char c) {
     if (z != -1) {
@@ -43,13 +47,14 @@ void cpu_set_flags(cpu_context *ctx, char z, char n, char h, char c) {
 // Load instruction
 static void proc_ld(cpu_context *ctx) {
     if (ctx->dest_is_mem) {
-        if (ctx->cur_inst->reg_2 >= RT_AF) {   // in the form: LD (BC), A (and 16-bit register)
+        if (is_16_bit(ctx->cur_inst->reg_2)) {   // in the form: LD (BC), A (and 16-bit register)
             emu_cycles(1);
             bus_write16(ctx->mem_dest, ctx->fetched_data);
         } else {
             bus_write(ctx->mem_dest, ctx->fetched_data);
         }
 
+        emu_cycles(1);
         return;
     }
 
@@ -74,7 +79,7 @@ static void proc_ldh(cpu_context *ctx) {
     if (ctx->cur_inst->reg_1 == RT_A) {
         cpu_set_reg(ctx->cur_inst->reg_1, bus_read(0xFF00 | ctx->fetched_data));
     } else {
-        bus_write(0xFF00 | ctx->fetched_data, ctx->regs.a);
+        bus_write(ctx->mem_dest, ctx->regs.a);
     }
 
     emu_cycles(1);
@@ -83,7 +88,7 @@ static void proc_ldh(cpu_context *ctx) {
 // XOR instruction
 static void proc_xor(cpu_context *ctx) {
     ctx->regs.a ^= ctx->fetched_data & 0xFF;    // XOR reg A and fetched data
-    cpu_set_flags(ctx, ctx->regs.a, 0, 0, 0);   // sets flags to (Z 0 0 0)
+    cpu_set_flags(ctx, ctx->regs.a == 0, 0, 0, 0);   // sets flags to (Z 0 0 0)
 }
 
 // Returns bool representing if condition is satisfied
@@ -188,6 +193,129 @@ static void proc_push(cpu_context *ctx) {
     emu_cycles(1);
 }
 
+// Inc instruction
+static void proc_inc(cpu_context *ctx) {
+    u16 val = cpu_read_reg(ctx->cur_inst->reg_1) + 1;
+
+    if (is_16_bit(ctx->cur_inst->reg_1)) {
+        emu_cycles(1);
+    }
+
+    if (ctx->cur_inst->reg_1 == RT_HL && ctx->cur_inst->mode == AM_MR) {
+        val = bus_read(cpu_read_reg(RT_HL)) + 1;
+        val &= 0xFF;
+        bus_write(cpu_read_reg(RT_HL), val);
+    } else{
+        cpu_set_reg(ctx->cur_inst->reg_1, val);
+        val = cpu_read_reg(ctx->cur_inst->reg_1);
+    }
+
+    if ((ctx->cur_opcode & 0x03) == 0x03) {
+        return;
+    }
+
+    cpu_set_flags(ctx, val == 0, 0, (val & 0x0F) == 0, -1);
+}
+
+// Dec instruction
+static void proc_dec(cpu_context *ctx) {
+    u16 val = cpu_read_reg(ctx->cur_inst->reg_1) - 1;
+
+    if (is_16_bit(ctx->cur_inst->reg_1)) {
+        emu_cycles(1);
+    }
+
+    if (ctx->cur_inst->reg_1 == RT_HL && ctx->cur_inst->mode == AM_MR) {
+        val = bus_read(cpu_read_reg(RT_HL)) - 1;
+        bus_write(cpu_read_reg(RT_HL), val);
+    } else{
+        cpu_set_reg(ctx->cur_inst->reg_1, val);
+        val = cpu_read_reg(ctx->cur_inst->reg_1);
+    }
+
+    if ((ctx->cur_opcode & 0x0B) == 0x0B) {
+        return;
+    }
+
+    cpu_set_flags(ctx, val == 0, 1, (val & 0x0F) == 0x0F, -1);
+}
+
+// Sub instruction
+static void proc_sub(cpu_context *ctx) {
+    u16 val = cpu_read_reg(ctx->cur_inst->reg_1) - ctx->fetched_data;
+    
+    int z = val == 0;
+    int h = ((int)cpu_read_reg(ctx->cur_inst->reg_1) & 0xF) - ((int)ctx->fetched_data & 0xF) < 0;
+    int c = ((int)cpu_read_reg(ctx->cur_inst->reg_1)) - ((int)ctx->fetched_data) < 0;
+
+    cpu_set_reg(ctx->cur_inst->reg_1, val);
+    cpu_set_flags(ctx, z, 1, h, c);
+}
+
+// Sbc instruction
+static void proc_sbc(cpu_context *ctx) {
+    u8 val = ctx->fetched_data + CPU_FLAG_C;
+
+    int z = cpu_read_reg(ctx->cur_inst->reg_1) - val == 0;
+
+    int h = ((int)cpu_read_reg(ctx->cur_inst->reg_1) & 0xF) 
+        - ((int)ctx->fetched_data & 0xF) - ((int)CPU_FLAG_C) < 0;
+    int c = ((int)cpu_read_reg(ctx->cur_inst->reg_1)) 
+        - ((int)ctx->fetched_data) - ((int)CPU_FLAG_C) < 0;
+
+    cpu_set_reg(ctx->cur_inst->reg_1, cpu_read_reg(ctx->cur_inst->reg_1) - val);
+    cpu_set_flags(ctx, z, 1, h, c);
+}
+
+// Adc instruction
+static void proc_adc(cpu_context *ctx) {
+    u16 u = ctx->fetched_data;
+    u16 a = ctx->regs.a;
+    u16 c = CPU_FLAG_C;
+
+    ctx->regs.a = (a + u + c) & 0xFF;
+
+    cpu_set_flags(ctx, ctx->regs.a == 0, 0,
+        (a & 0xF) + (u & 0xF) + c > 0xF,
+        a + u + c > 0xFF);
+}
+
+// Add instruction
+static void proc_add(cpu_context *ctx) {
+    u32 val = cpu_read_reg(ctx->cur_inst->reg_1) + ctx->fetched_data;
+
+    bool is_16bit = is_16_bit(ctx->cur_inst->reg_1);
+
+    if (is_16bit) {
+        emu_cycles(1);
+    }
+
+    if (ctx->cur_inst->reg_1 == RT_SP) {
+        val = cpu_read_reg(ctx->cur_inst->reg_1) + (char)ctx->fetched_data;
+    }
+
+    int z = (val & 0xFF) == 0;
+    int h = (cpu_read_reg(ctx->cur_inst->reg_1) & 0xF) + (ctx->fetched_data & 0xF) >= 0x10;
+    int c = (int)(cpu_read_reg(ctx->cur_inst->reg_1) & 0xFF) + (int)(ctx->fetched_data & 0xFF) >= 0x100;
+
+    if (is_16bit) {
+        z = -1;
+        h = (cpu_read_reg(ctx->cur_inst->reg_1) & 0xFFF) + (ctx->fetched_data & 0xFFF) >= 0x1000;
+        u32 n = ((u32)cpu_read_reg(ctx->cur_inst->reg_1)) + ((u32)ctx->fetched_data);
+        c = n >= 0x10000;
+    }
+
+    if (ctx->cur_inst->reg_1 == RT_SP) {
+        z = 0;
+        h = (cpu_read_reg(ctx->cur_inst->reg_1) & 0xF) + (ctx->fetched_data & 0xF) >= 0x10;
+        c = (int)(cpu_read_reg(ctx->cur_inst->reg_1) & 0xFF) + (int)(ctx->fetched_data & 0xFF) > 0x100;
+    }
+
+    cpu_set_reg(ctx->cur_inst->reg_1, val & 0xFFFF);
+    cpu_set_flags(ctx, z, 0, h, c);
+    
+}
+
 static IN_PROC processors[] = {
     [IN_NONE] = proc_none,
     [IN_NOP] = proc_nop,
@@ -202,6 +330,12 @@ static IN_PROC processors[] = {
     [IN_RET] = proc_ret,
     [IN_RETI] = proc_reti,
     [IN_RST] = proc_rst,
+    [IN_DEC] = proc_dec,
+    [IN_INC] = proc_inc,
+    [IN_ADD] = proc_add,
+    [IN_ADC] = proc_adc,
+    [IN_SUB] = proc_sub,
+    [IN_SBC] = proc_sbc,
     [IN_XOR] = proc_xor
 };
 
